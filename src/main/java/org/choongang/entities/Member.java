7bb70768678c6f8b0c8bb2869dde42833c9e137b
allely <ggrocky49@gmail.com>
2024-01-02 09:15:51 +0900

    DDD 설계로 변경

---
+++ src/main/java/org/choongang/entities/Member.java
@@ -0,0 +1,11 @@
+package org.choongang.entities;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@Entity
+public class Member extends Base{
+}
